c988551779124603b320dd12b774f4d6d61ec03a
joshhacksthings <joshua.nowak@asu.edu>
2019-06-11 08:24:42 -0700

    add route, cursory tests

--- beer_me/hello.py
+++ beer_me/hello.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'index'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World. This is a test.'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
