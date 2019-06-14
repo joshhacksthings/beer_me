4f3c277c5ff0760e5b348d824845014ad4d47673
joshhacksthings <joshua.nowak@asu.edu>
2019-06-13 19:39:42 -0700

    reformat main, remove CORS library use

--- beer_me/main.py
+++ beer_me/main.py
@@ -1,42 +1,21 @@
-import os
+from flask import Flask, request, \
+    render_template, url_for, redirect
+app = Flask(__name__)
 
-from flask import Flask, jsonify, render_template
-from flask_cors import CORS
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
 
 
-def create_app(config=None):
-    app = Flask(__name__)
-
-    # See http://flask.pocoo.org/docs/latest/config/
-    app.config.update(dict(DEBUG=True))
-    app.config.update(config or {})
-
-    # Setup cors headers to allow all domains
-    # https://flask-cors.readthedocs.io/en/latest/
-    CORS(app)
-
-    # Definition of the routes. Put them into their own file. See also
-    # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
-    @app.route("/")
-    def hello_world():
-        return "Hello World"
-
-    @app.route('/greet')
-    def greet():
-        user_info = {'username': 'Josh', 'age': "20"}
-        return render_template(
-            'user_greet.html',
-            user=user_info['username'],
-            age=user_info['age'])
-
-    @app.route("/foo/<someId>")
-    def foo_url_arg(someId):
-        return jsonify({"echo": someId})
-
-    return app
+@app.route('/greet')
+def greet():
+    user_info = {'username': 'Josh', 'age': "20"}
+    return render_template(
+        'user_greet.html',
+        user=user_info['username'],
+        age=user_info['age'])
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app = create_app()
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=8080)
